33ee5b0610dc74dfea39c12a30cbd3c06cd53e9d
Farida <faridah.absi@gmail.com>
2018-10-07 13:31:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-class boardMembers {
-  
+class BoardMember{
   constructor(name, homeState, training){
-  this.name = name;
-  this.homeState = homeState;
-  this.training = training;
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
   }
-  
- veto(){
-     return "No, I must disagree"
+  veto(){
+    return 'No, I must disagree'
   }
- approve(){
-    return "You can do that!"
-  
-   } 
-   
-   doCharity(){
+  approve(){
+    return 'You can do that!'
+  }
+  doCharity(){
     return "I like to help people."
   }
-
    releasePressStatement(){
     return "You will see great things from Scuber."
   }
-
    sayHi(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-   }
-}
-
-class Ceo extends boardMembers{
- sayHello(){
-    return `Welcome aboard!`
   }
 }
+ class Ceo extends BoardMember{
+  hireEmployee(){
+    return 'Welcome aboard!';
+  }
+} 
